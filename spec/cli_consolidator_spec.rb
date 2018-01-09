18f6b5c799c8ed64acba538c2ba31d1803b017c6
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2018-01-09 14:31:33 -0700

    Update name of test data file

--- spec/cli_consolidator_spec.rb
+++ spec/cli_consolidator_spec.rb
@@ -15,8 +15,8 @@ describe Proptax::CLI, :type => :aruba do
 
     it "produces the correct output when provided a valid directory" do
       FileUtils.cp_r 'spec/aruba_data', 'tmp/aruba/aruba_data'
-      # `#read` leaves a `\n`, `aruba` does not
-      expected = File.read('spec/aruba_data/expected.csv').strip
+      # `#read` leaves a `\n`, while `aruba` does not
+      expected = File.read('spec/aruba_data/expected_consolidated.csv').strip
     
       run_simple "proptax consolidate aruba_data"
       expect(last_command_started).to be_successfully_executed
